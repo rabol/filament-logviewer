4f860abfa7c74714f4a9a93fca75dd43583b0e96
rabol <steen@rabol.com>
2022-01-02 20:04:50 +0100

    Created traits and classes to eliminate the use of Spatie package tool

--- src/FilamentLogviewerServiceProvider.php
+++ src/FilamentLogviewerServiceProvider.php
@@ -2,12 +2,59 @@
 
 namespace Rabol\FilamentLogviewer;
 
-use Spatie\LaravelPackageTools\Package;
-use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Filament\PluginServiceProvider;
+use Rabol\Pages\LogViewerPage;
+use Rabol\FilamentLogviewer\Classes\Package;
+use Rabol\FilamentLogviewer\Traits\ServiceProvider;
 use Rabol\FilamentLogviewer\Commands\FilamentLogviewerCommand;
 
-class FilamentLogviewerServiceProvider extends PackageServiceProvider
+class FilamentLogviewerServiceProvider extends PluginServiceProvider
 {
+    use ServiceProvider;
+
+    public static string $name = 'log-viewer';
+ 
+    protected function getPages(): array
+    {
+        return [
+            LogViewerPage::class,
+        ];
+    }
+
+    protected function getWidgets(): array
+    {
+        return [
+            LogViewerWidget::class,
+        ];
+    }
+
+    protected function getStyles(): array
+    {
+        return [
+            //'my-package-styles' => __DIR__ . '/../dist/app.css',
+        ];
+    }
+
+    protected function getScripts(): array
+    {
+        return [
+            'my-package-scripts' => __DIR__ . '/../dist/app.js',
+        ];
+    }
+
+    protected function getScriptData(): array
+    {
+        return [
+            //'userId' => Auth::id(),
+        ];
+    }
+
+    protected function getCommands(): array
+    {
+        return [
+            //Commands\CustomCommand::class,
+        ];
+    }
     public function configurePackage(Package $package): void
     {
         /*
---
+++ src/Pages/LogViewerPage.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Rabol\Pages;
+
+class LogViewerPage
+{
+    
+}
